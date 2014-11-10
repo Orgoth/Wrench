88174848a552003911f3a44f62b5f31d1d357689
Axel Venet <axel-venet@developtech.fr>
2014-11-10 22:55:48 +0100

    Add encoding functions for data handling through pipeline

--- lib/Wrench/Application/Application.php
+++ lib/Wrench/Application/Application.php
@@ -30,5 +30,36 @@ abstract class Application
      * @param Payload $payload A payload object, that supports __toString()
      * @param Connection $connection
      */
-	abstract public function onData($payload, $connection);
+    abstract public function onData($payload, $connection);
+        
+    protected function _decodeData($data)
+    {
+        $decodedData = json_decode($data, true);
+        if($decodedData === null)
+        {
+                return false;
+        }
+
+        if(isset($decodedData['action'], $decodedData['data']) === false)
+        {
+                return false;
+        }
+
+        return $decodedData;
+    }
+
+    protected function _encodeData($action, $data)
+    {
+        if(empty($action))
+        {
+                return false;
+        }
+
+        $payload = array(
+                'action' => $action,
+                'data' => $data
+        );
+
+        return json_encode($payload);
+    }
 }
